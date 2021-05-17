e2f9584c7d8909bf94fe5b848418d441e2392fdf
redallen <zallen@redhat.com>
2021-05-17 14:06:00 -0400

    initial commit

---
+++ background.js
@@ -0,0 +1,25 @@
+chrome.contextMenus.create(
+  {
+    title: 'root',
+    id: 'menu-0'
+  }
+);
+
+for (let i = 1; i < 31; i++) {
+  chrome.contextMenus.create(
+    {
+      id: `menu-${i}`,
+      parentId: `menu-${i - 1}`,
+      title: `Menu ${i} fddfdfdfjkhslahdlash`,
+    }
+  );
+  for (let j = 0; j < i; j++) {
+    chrome.contextMenus.create(
+      {
+        id: `menu-${i}-${j}`,
+        parentId: `menu-${i - 1}`,
+        title: `Menu ${i} item ${j}`
+      }
+    );
+  }
+}
